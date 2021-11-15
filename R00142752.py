e5b85851bd63c7bbf1032b298bbfa2e6a0e337ea
paudieMola <44343590+paudieMola@users.noreply.github.com>
2021-11-15 11:03:15 +0000

    Task 3 nearly done

--- R00142752.py
+++ R00142752.py
@@ -39,14 +39,48 @@ def Task1():
 
 
 def Task2():
-    #at least 1 movie longer than 150 and lang not eng
-    return
-  
-
-
+    #target is at least 1 movie longer than 150 and lang not eng
+    #identify films with target language and duration
+    #print(df['language'].value_counts())
+    lengthMask = df['duration'] > 150
+    langMask = df['language'] != 'English'
+    target = df[langMask & lengthMask]
+    #print(len(target))
+    #group those by country
+    targetCountries = target.groupby(['country'])
+    #calculate how many films from each country and print with more than 1
+    filmTarCountry = targetCountries.size()
+    print('Films with at least 2 non English language films of greater than 150m in duration:')
+    print(filmTarCountry[filmTarCountry > 1])
+    
 
 def Task3():
-   return
+   #sort by years early to latest and show income(gross) growth
+   #fill in nan gross with average. 
+   #to get an idea of what values are in the gross column
+   #print(df['gross'].describe())
+   #nullGross = [df['gross'].isnull()
+   #to make sure null values are not used in calculating mean
+   #grossMean1 = df['gross'].mean(skipna = True)
+   #grossMean = df['gross'].mean()
+   #print(grossMean1, grossMean)
+   
+   grossMean = df['gross'].mean()
+   print(grossMean)
+   df['gross'].fillna(value=df['gross'].mean(), inplace = True)
+   #print(df.info()) 
+   yearly = df.groupby(['title_year', 'gross'])['gross'].sum()
+   print(type(yearly))
+   #yearly.plot(kind = 'scatter', x = 'title_year', y = 'gross')
+   #plt.show()
+   # print(df['gross'].describe())
+   #plt.plot(yearly)
+   #plot.show()
+   yearly.plot()
+   #plt.show()
+   
+   
+   
     
    
     
@@ -72,6 +106,6 @@ def Task6():
     return
     
 
-Task1()
+Task3()
 
 
